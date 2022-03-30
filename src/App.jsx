a5b43b348f01a12403958b9a64b20fa35f076900
NhanHeio <hotrungnhan2000@gmail.com>
2022-03-30 23:09:32 +0700

    setup ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import AppLayout from './layout/AppLayout';
 import './App.css';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const usePageViews = () => {
+  let location = useLocation()
+  useEffect(() =>{
+    if(!window.GA_INITIALIZED) {
+      ReactGA.initialize(TRACKING_ID)
+      window.GA_INITIALIZED = true
+    }
+    ReactGA.set({ path: location.pathname})
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  },[location]);
+}
 
 function App() {
+  usePageViews()
   return (
     <div className="App">
       <AppLayout />
