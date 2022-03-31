369a47723dec77a425a25955b9b967301043c21c
NhanHeio <hotrungnhan2000@gmail.com>
2022-03-31 17:19:52 +0700

    add ga4 17:19 31/03

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,18 @@ import React, { useEffect } from 'react';
 import ContactComponent from '../components/contact/ContactComponent';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import { useLocation } from 'react-router-dom';
+import ReactGA from "react-ga4";
 
 const Contact = () => {
+   // ga4
+   let location = useLocation()
+   useEffect(() => {
+     ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_CODE)
+     ReactGA.send({ hitType: "pageview", page: location.pathname })
+   }, [location]);
+ 
+   //
   useEffect(() => {
     document.title = 'Contact'
   }, [])
